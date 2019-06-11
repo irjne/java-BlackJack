1dfc0ee0aee787ee9a7a845a95520f2bbb95cf75
irjne <49485862+irjne@users.noreply.github.com>
2019-06-11 11:42:55 +0200

    Add files via upload

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,153 @@
 package sample;
-import java.util.TreeSet;
-import javafx.scene.control.TextField;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class Controller {
-    private Game game = new Game();
-    private Player player = new Player(50.000);
 
     @FXML
-    private void deal () {
-        //inserimento delle opzioni dello start
+    private VBox vbox;
 
-        game.betAndGetCards(player, 5);
+    @FXML
+    private Pane table;
+
+    @FXML
+    private Button bet5;
+
+    @FXML
+    private Button bet10;
+
+    @FXML
+    private Button bet50;
+
+    @FXML
+    private Button bet100;
+
+    @FXML
+    private Button betallin;
+
+    @FXML
+    private Button btnHit;
+
+    @FXML
+    private Label lblCredit;
+
+    @FXML
+    private Label credit;
+
+    @FXML
+    private Pane cardD1;
+
+    @FXML
+    private Label cardValue;
+
+    @FXML
+    private ImageView cardSeed;
+
+    @FXML
+    private Label cardValue1;
+
+    @FXML
+    private ImageView cardSeed1;
+
+    @FXML
+    private Label cardValue2;
+
+    @FXML
+    private ImageView cardSeed2;
+
+    @FXML
+    private Label cardValue11;
+
+    @FXML
+    private ImageView cardSeed11;
+
+    @FXML
+    private Label cardValue111;
+
+    @FXML
+    private ImageView cardSeed111;
+
+    @FXML
+    private Label cardValue1111;
+
+    @FXML
+    private ImageView cardSeed1111;
+
+    @FXML
+    private Label cardValue3;
+
+    @FXML
+    private ImageView cardSeed3;
+
+    @FXML
+    private Label cardValue12;
+
+    @FXML
+    private ImageView cardSeed12;
+
+    @FXML
+    private Label cardValue21;
+
+    @FXML
+    private ImageView cardSeed21;
+
+    @FXML
+    private Label cardValue112;
+
+    @FXML
+    private ImageView cardSeed112;
+
+    @FXML
+    private Label cardValue1112;
+
+    @FXML
+    private ImageView cardSeed1112;
+
+    @FXML
+    private Label cardValue11111;
+
+    @FXML
+    private ImageView cardSeed11111;
+
+    @FXML
+    void bet10(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bet100(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bet5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bet50(ActionEvent event) {
+
+    }
+
+    @FXML
+    void betAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stand(ActionEvent event) {
+
     }
 
 }
+
